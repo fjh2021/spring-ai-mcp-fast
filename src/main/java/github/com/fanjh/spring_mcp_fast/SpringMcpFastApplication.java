000dda551b6d427bf64483ae4488c86f7b2ca6da
fjh2021 <1731970489@qq.com>
2025-06-21 09:35:51 +0800

    Initial commit Spring Initializr

---
+++ src/main/java/github/com/fanjh/spring_mcp_fast/SpringMcpFastApplication.java
@@ -0,0 +1,13 @@
+package github.com.fanjh.spring_mcp_fast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMcpFastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMcpFastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/com/fanjh/spring_mcp_fast/SpringMcpFastApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.fanjh.spring_mcp_fast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcpFastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
